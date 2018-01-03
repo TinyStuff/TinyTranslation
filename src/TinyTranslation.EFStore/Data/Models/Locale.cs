0461ba8c35b7060602e80f23c84aacce83a80c6f
mats tornberg <mats.tornberg@gmail.com>
2018-01-03 22:45:34 +0100

    ef base

---
+++ src/TinyTranslation.EFStore/Data/Models/Locale.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace TinyTranslation.EFStore.Data.Models
+{
+    public class Locale
+    {
+        public int LocalID { get; set; }
+        public string Key { get; set; }
+    }
+}
---
+++ src/TinyTranslation.EFStore/Data/Models/Translation.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace TinyTranslation.EFStore.Data.Models
+{
+    public class Translation
+    {
+        public int TranslationID { get; set; }
+        public int LocalID { get; set; }
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+}
