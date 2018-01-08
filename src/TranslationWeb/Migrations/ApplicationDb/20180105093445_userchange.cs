70c8392222e5bf5dc5b1751d1221ba033a9fca64
mats tornberg <mats.tornberg@gmail.com>
2018-01-08 10:12:05 +0100

    db

---
+++ src/TranslationWeb/Migrations/ApplicationDb/20180105093445_userchange.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace TranslationWeb.Migrations.ApplicationDb
+{
+    public partial class userchange : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
